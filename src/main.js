56849b594a014f738a09cd3bcc2da287baea3afb
Donny Kurnia <donnykurnia@gmail.com>
2022-11-23 08:16:17 +0700

    fix data in wrong column, use Utilities.parseCsv method

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
 
+// global variables
+var cc = DataStudioApp.createCommunityConnector();
+var csv_data = undefined;
+
 function sendUserError(message) {
-  var cc = DataStudioApp.createCommunityConnector();
   cc.newUserError().setText(message).throwException();
 }
 
+// https://developers.google.com/datastudio/connector/reference#getauthtype
 function getAuthType() {
   var response = { type: 'NONE' };
   return response;
 }
 
 function getConfig(request) {
-  var communityConnector = DataStudioApp.createCommunityConnector();
-  var connectorConfig = communityConnector.getConfig();
+  var connectorConfig = cc.getConfig();
   
   connectorConfig.setDateRangeRequired(false);
     
   connectorConfig.newTextInput()
     .setId('url')
-    .setName('Enter the URL of your CSV');
+    .setName('Enter the URL of your CSV')
+    .setHelpText('e.g. https://api.site.com/api/data.json')
+    .setPlaceholder('https://');
   
   connectorConfig.newSelectSingle()
     .setId('delimiter')
@@ -26,14 +31,6 @@ function getConfig(request) {
     .addOption(connectorConfig.newOptionBuilder().setLabel('Comma').setValue(','))
     .addOption(connectorConfig.newOptionBuilder().setLabel('Semicolon').setValue(';'))
     .addOption(connectorConfig.newOptionBuilder().setLabel('Tabulation').setValue('\t'));
-  
-  connectorConfig.newSelectSingle()
-    .setId('textQualifier')
-    .setName('Does the values have a text qualifier?')
-    .setAllowOverride(false)
-    .addOption(connectorConfig.newOptionBuilder().setLabel('No value').setValue('undefined'))
-    .addOption(connectorConfig.newOptionBuilder().setLabel('Simple quote').setValue("'"))
-    .addOption(connectorConfig.newOptionBuilder().setLabel('Double quote').setValue('"'));
   
   connectorConfig.newSelectSingle()
     .setId('containsHeader')
@@ -45,129 +42,128 @@ function getConfig(request) {
   return connectorConfig.build();
 }
 
-function findLineSeparator(content) {
-  if (!content) {
-    return undefined;
-  }
-  if (content.indexOf('\r\n') >= 0) { // Windows
-    return '\r\n';
-  } else if (content.indexOf('\r') >= 0) { // MacOS
-    return '\r';
-  } else if (content.indexOf('\n') >= 0) { // Linux / OSX
-    return '\n';
-  } else {
-    return undefined;
-  }
-}
-
-function fetchData(url) {
-  if (!url || !url.match(/^https?:\/\/.+$/g)) {
+/* this method return a two-dimensional array containing the values in the CSV string */
+function getCSVData(request, delimiter) {
+  if (!request.configParams.url || !request.configParams.url.match(/^https?:\/\/.+$/g)) {
     sendUserError("Input error: Invalid URL");
   }
-  var response = UrlFetchApp.fetch(url);
+  var params = {
+    headers: JSON.parse(request.configParams.http_headers)
+  };
+  var response = UrlFetchApp.fetch(request.configParams.url, params);
   var content = response.getContentText();
   if (!content) {
     sendUserError("Error during parsing content: Empty content");
   }
-  return content;
+  return Utilities.parseCsv(content, delimiter);
 }
 
-function getFields(request, content) {
-  var communityConnector = DataStudioApp.createCommunityConnector();
-  var fields = communityConnector.getFields();
-  var types = communityConnector.FieldType;
-  var textQualifier = request.configParams.textQualifier;
-  var containsHeader = request.configParams.containsHeader;
-  
-  var lineSeparator = findLineSeparator(content);
-  var firstLineContent;
-  if (lineSeparator) {
-    firstLineContent = content.substring(0, content.indexOf(lineSeparator));
-  } else {
-    firstLineContent = content;
+function getFields(request) {
+  var fields = cc.getFields();
+  var types = cc.FieldType;
+  var aggregations = cc.AggregationType;
+
+  var containsHeader = request.configParams.containsHeader === 'true';
+  var dataFields = [];
+
+  if ( csv_data === undefined ) {
+    csv_data = getCSVData(request, request.configParams.delimiter);
   }
-  var valueSeparator = request.configParams.delimiter;
-  if (textQualifier !== 'undefined') {
-    firstLineContent = firstLineContent.substring(1, firstLineContent.length - 1);
-    separator = textQualifier + valueSeparator + textQualifier;
-  }
-  var firstLineColumns = firstLineContent.split(valueSeparator);
-  
-  var i=1;
+
+  var firstLineColumns = csv_data[0];
+  var i = 1;
   firstLineColumns.forEach(function(value) {
-    var field = fields.newDimension().setType(types.TEXT);
-    if (containsHeader === 'true') {
-      // because Id can't have space
-      field.setId(value.replace(/\s/g, '_').toLowerCase());
-      field.setName(value);
+    var key;
+    if (containsHeader) {
+      key = value.replace(/\s/g, '_');
+      dataFields.push({ key: key, type: "string", name: value});
     } else {
-      field.setId('column_' + i);
+      key = 'column_' + i;
+      dataFields.push({ key: key, type: "string"});
       i++;
     }
   });
-  
+
+  //first line of data, the only possible place to set number type
+  var firstLineData = csv_data[0];
+  if (containsHeader) {
+    firstLineData = csv_data[1];
+  }
+  firstLineData.forEach(function(value, index) {
+    if ( !isNaN(value) ) {
+      dataFields[index].type = "number";
+    }
+  });
+  //the rest of data, to turn number back to string if the data is not numeric
+  var slice = 1;
+  if (containsHeader) {
+    slice = 2;
+  }
+  csv_data.slice(slice).forEach(function(row) {
+    row.forEach(function(value, index) {
+      if ( dataFields[index].type === "number" && isNaN(value) ) {
+        dataFields[index].type = "string";
+      }
+    });
+  });
+
+  dataFields.forEach(function(dataField) {
+    var key = dataField.key;
+    var field;
+    if ( dataField.type === "number" ) {
+      field = fields.newMetric().setId(key).setType(types.NUMBER).setAggregation(aggregations.SUM);
+    } else {
+      field = fields.newDimension().setId(key).setType(types.TEXT);
+    }
+    if ( dataField.name !== undefined ) {
+      field.setName(dataField.name);
+    }
+  });
   return fields;
 }
 
 function getSchema(request) {
-  var content = fetchData(request.configParams.url);
-  var fields = getFields(request, content).build();
+  var fields = getFields(request).build();
   return { schema: fields };
 }
 
 function getData(request) {
-  var content = fetchData(request.configParams.url);
   var requestedFieldIds = request.fields.map(function(field) {
     return field.name;
   });
-  var fields = getFields(request, content);
+  var fields = getFields(request);
   var requestedFields = fields.forIds(requestedFieldIds);
+
+  if ( csv_data === undefined ) {
+    csv_data = getCSVData(request, request.configParams.delimiter);
+  }
+
   var buildedFields = fields.build();
-  
-  var requestedFieldsIndex = buildedFields.reduce(function(filtered, field, index) {
-    if (requestedFieldIds.indexOf(field.name) >=0 ) {
-      filtered.push(index);
-    }
-    return filtered;
-  }, []);
-  
-  var textQualifier = request.configParams.textQualifier;
-  var delimiter = request.configParams.delimiter;
-  var containsHeader = request.configParams.containsHeader;
-  
-  var lineSeparator = findLineSeparator(content);
-  var contentRows;
-  if (lineSeparator) {
-    contentRows = content.split(lineSeparator);
-  } else {
-    contentRows = [content];
-  }
-  var valueSeparator = delimiter;
-  if (textQualifier !== 'undefined') {
-    valueSeparator = textQualifier + valueSeparator + textQualifier;
-  }
-  
-  var rows = contentRows.map(function(contentRow) {
-    if (textQualifier !== 'undefined') {
-      contentRow = contentRow.substring(1, contentRow.length - 1);
-    }
-    var allValues = contentRow.split(valueSeparator);
-    if (buildedFields.length !== allValues.length) {
+  var fieldNameIndex = buildedFields.reduce(function(result, field, index) {
+    result[field.name] = index;
+    return result;
+  }, {});
+
+  var requestedFieldsIndex = requestedFieldIds.map(function(fieldId) {
+    return fieldNameIndex[fieldId];
+  });
+
+  var rows = csv_data.map(function(row) {
+    if (buildedFields.length !== row.length) {
       sendUserError("Error during parsing content: the number of columns on each row is not respect");
     }
-    var requestedValues = allValues.filter(function(value, index) {
-      return requestedFieldsIndex.indexOf(index) >= 0;
+    var requestedValues = [];
+    requestedFieldsIndex.forEach(function(colIndex) {
+      requestedValues.push(row[colIndex]);
     });
     return { values: requestedValues };
   });
-  if (containsHeader === 'true') {
+  if (request.configParams.containsHeader === 'true') {
     rows = rows.slice(1);
   }
-  
-  var result = {
+
+  return {
     schema: requestedFields.build(),
     rows: rows
   };
-  
-  return result;
 }
